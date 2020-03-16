9a8b10e92c7181eded5f16a0dae14befae491e39
Nicolas <nicolas.lecrique+git@globers.co>
2020-03-16 23:00:58 +0100

    init spring boot api project

---
+++ api/src/main/kotlin/co/globers/ibo/IboApplication.kt
@@ -0,0 +1,11 @@
+package co.globers.ibo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IboApplication
+
+fun main(args: Array<String>) {
+	runApplication<IboApplication>(*args)
+}
---
+++ api/src/test/kotlin/co/globers/ibo/IboApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.globers.ibo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IboApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
