8304a2637d82e7810038b683104fb3f9b2bba358
Nicolas <nicolas.lecrique+git@globers.co>
2020-03-19 23:19:33 +0100

    allow all CORS requests to back
    temp: should be more fine restrictive at target

---
+++ back/src/main/kotlin/co/globers/ibo/back/WebConfig.kt
@@ -0,0 +1,17 @@
+package co.globers.ibo.back
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+// to remove all CORS constraint, to do: to make more fine-grained
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
+}
