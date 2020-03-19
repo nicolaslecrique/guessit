04db027bf0cde239e72765c68f0abe77e1c98897
Nicolas <nicolas.lecrique+git@globers.co>
2020-03-19 23:19:42 +0100

    remove useless import

--- back/src/main/kotlin/co/globers/ibo/back/MlRestService.kt
+++ back/src/main/kotlin/co/globers/ibo/back/MlRestService.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
 
 import org.springframework.web.client.RestTemplate
-import java.util.*
 
 @Service
 class MlRestService(restTemplateBuilder: RestTemplateBuilder, iboConfig: IboConfig) {
