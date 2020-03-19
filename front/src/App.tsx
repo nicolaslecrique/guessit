10c8d6d670718991970770386898934500615145
Nicolas <nicolas.lecrique+git@globers.co>
2020-03-19 23:20:51 +0100

    make request to backend from front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import backUrl from "./backUrl"
 
 function App() {
-  return (
+
+    // TODO NICO: GERER CORS SUR LE BACKEND
+    // Blocage d’une requête multiorigines (Cross-Origin Request) : la politique « Same Origin » ne permet pas de consulter la ressource distante située sur http://localhost:8080/compute_guesses. Raison : échec de la requête CORS.
+
+    async function computeGuesses() {
+        const response = await fetch(`${backUrl}/compute_guesses`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ description_sentences: ['sentence-1', 'sentence-2'] }),
+        })
+        console.log(await response.json())
+    }
+
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={computeGuesses}>Call backend</button>
       </header>
     </div>
   );
