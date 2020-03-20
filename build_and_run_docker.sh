c68543fca2896750bed3200b9f4af7f7b02b2a4d
Nicolas <nicolas.lecrique+git@globers.co>
2020-03-20 12:08:51 +0100

    backup docker config for front (delete later)

--- build_and_run_docker.sh
+++ build_and_run_docker.sh
@@ -1,4 +1,11 @@
 cd back
-./build_docker_image.sh
+./gradlew build
 cd ..
+
+cd front
+npm run build
+cd..
+
+# NB: nothing to "build" for ml service before we build container
+
 docker-compose up --build -d
