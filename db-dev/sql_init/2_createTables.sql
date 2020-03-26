43e9bfe26f5b0dce43f65446e38dff33135aa6c2
Nicolas <nicolas.lecrique+git@globers.co>
2020-03-26 16:33:56 +0100

    add comment to db table creation script
    explain conventions

--- db-dev/sql_init/2_createTables.sql
+++ db-dev/sql_init/2_createTables.sql
@@ -2,6 +2,10 @@
 USE ibo;
 SET NAMES utf8;
 
+-- All tables has
+-- id: primary_key, int, for join requests and foreign keys, should not be exposed to service clients
+-- uri: unique identifier, immutable, can be exposed
+-- creation_datetime: for diagnostics
 
 CREATE TABLE entity_to_guess(
 	id INT NOT NULL AUTO_INCREMENT,
