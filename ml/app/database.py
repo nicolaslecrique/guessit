611aebf58cb0d51e4812936a96a47917ce9be41f
Mokhtari Mohamed <mohamed.mokhtari2@gmail.com>
2020-04-11 12:07:01 +0200

    Use a context manager to connect/create cursor/commit and close them

--- ml/app/database.py
+++ ml/app/database.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from typing import Dict
 
 import numpy as np
@@ -6,62 +7,55 @@ import psycopg2
 from app.models import Entity
 
 
+@contextmanager
 def connect():
     # Calling connect with an empty string uses PG* environment variables as parameters
     # https://www.psycopg.org/docs/module.html#psycopg2.connect
     # https://github.com/psycopg/psycopg2/issues/767
-    return psycopg2.connect('')
+    connection = psycopg2.connect('')
+    cursor = connection.cursor()
+
+    try:
+        yield cursor
+    finally:
+        connection.commit()
+
+        cursor.close()
+        connection.close()
 
 def get_all_entities() -> Dict[str, Entity]:
-    connection = connect()
-    cursor = connection.cursor()
+    with connect() as cursor:
+        query = ("SELECT entity.id, entity.uri, sentence.sentence, sentence.sentence_embedding "
+                 "FROM ibo_ml.entity_to_guess entity "
+                 "JOIN ibo_ml.sentence_embedding sentence "
+                 "ON entity.id = sentence.entity_to_guess_id")
+        cursor.execute(query)
 
-    query = ("select entity.id, entity.uri, sentence.sentence, sentence.sentence_embedding "
-             "from ibo_ml.entity_to_guess entity "
-             "join ibo_ml.sentence_embedding sentence on entity.id = sentence.entity_to_guess_id")
-    cursor.execute(query)
+        entities = dict()
+        for (entity_id, entity_uri, sentence, sentence_embedding) in cursor:
+            entity = entities.setdefault(entity_uri, Entity(db_id=entity_id, uri=entity_uri))
 
-    entities = dict()
-    for (entity_id, entity_uri, sentence, sentence_embedding) in cursor:
-        entity = entities.setdefault(entity_uri, Entity(db_id=entity_id, uri=entity_uri))
+            embedding_np = np.asarray([sentence_embedding])
 
-        embedding_np = np.asarray([sentence_embedding])
+            entity.append_sentence_embedding(sentence, embedding_np)
 
-        entity.append_sentence_embedding(sentence, embedding_np)
-
-    cursor.close()
-    connection.close()
-
-    return entities
+        return entities
 
 def insert_sentence_embedding(sentence: str, embedding: np.ndarray, entity_id: int) -> None:
-    connection = connect()
-    cursor = connection.cursor()
+    with connect() as cursor:
+        query = ("INSERT INTO ibo_ml.sentence_embedding (sentence, sentence_embedding, entity_to_guess_id) "
+                 "VALUES (%s, %s, %s)")
+        data = (sentence, embedding.tolist(), entity_id)
 
-    query = "INSERT INTO ibo_ml.sentence_embedding (sentence, sentence_embedding, entity_to_guess_id) VALUES (%s, %s, %s)"
-    data = (sentence, embedding.tolist(), entity_id)
-
-    cursor.execute(query, data)
-
-    connection.commit()
-
-    cursor.close()
-    connection.close()
+        cursor.execute(query, data)
 
 def insert_entity(uri: str) -> int:
-    connection = connect()
-    cursor = connection.cursor()
+    with connect() as cursor:
+        query = "INSERT INTO ibo_ml.entity_to_guess(uri) VALUES (%s) RETURNING id;"
+        data = (uri,)
 
-    query = "INSERT INTO ibo_ml.entity_to_guess(uri) VALUES (%s) RETURNING id;"
-    data = (uri,)
+        cursor.execute(query, data)
 
-    cursor.execute(query, data)
+        entity_id = cursor.fetchone()[0]
 
-    entity_id = cursor.fetchone()[0]
-
-    connection.commit()
-
-    cursor.close()
-    connection.close()
-
-    return entity_id
+        return entity_id
