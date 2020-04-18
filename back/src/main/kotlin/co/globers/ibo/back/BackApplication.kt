1ad9021a7d50d4878a20a36728f4ecc350089de4
Nicolas <nicolas.lecrique+git@globers.co>
2020-04-18 17:51:03 +0200

    remove old comments

--- back/src/main/kotlin/co/globers/ibo/back/BackApplication.kt
+++ back/src/main/kotlin/co/globers/ibo/back/BackApplication.kt
@@ -9,8 +9,3 @@ class BackApplication
 fun main(args: Array<String>) {
 	runApplication<BackApplication>(*args)
 }
-
-
-// TODO: Faire en sorte d'utiliser la variable d'env PORT pour là ou écoute le process (plutot que 8080 par défaut)
-
-// TODO: 1) servir une route hello-world   2) faire un docker        3 ) script deployement cloud run
