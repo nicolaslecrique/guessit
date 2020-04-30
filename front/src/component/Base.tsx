c0b970a24ab32c670c3c3ca6841f526370197ace
Nicolas <nicolas.lecrique+git@globers.co>
2020-04-30 11:18:08 +0200

    Make typography size responsive
    cf. https://material-ui.com/customization/theming/#responsivefontsizes-theme-options-theme

--- front/src/component/Base.tsx
+++ front/src/component/Base.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { aboutRoute } from '../core/Routing'
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import {Typography, Theme} from "@material-ui/core"
+import {Typography, Theme, responsiveFontSizes} from "@material-ui/core"
 
 type BaseProps = {
   children: React.ReactNode
@@ -14,11 +14,10 @@ type BaseState = {
 }
 
 
-const theme: Theme = createMuiTheme({
+const theme: Theme = responsiveFontSizes(createMuiTheme({
   typography: {
-    fontSize: 12,
   }
-});
+}));
 
 class Base extends React.Component<BaseProps, BaseState> {
   constructor(props: BaseProps) {
