7eee575972a8faa2c082e768fa38836ab179df32
Nicolas <nicolas.lecrique+git@globers.co>
2020-04-30 12:47:12 +0200

    design intro (expect conversation)

--- front/src/pages/Intro.tsx
+++ front/src/pages/Intro.tsx
@@ -3,27 +3,71 @@ import { Link } from 'react-router-dom'
 import { Discussion, MessageProps } from '../component/Discussion'
 import { boardRoute } from '../core/Routing'
 import {Typography} from "@material-ui/core"
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-function Intro(): JSX.Element {
-  let messages: MessageProps[] = [
+const demoMessages: MessageProps[] = [
     { author: "Human", message: "It's a robot" },
     { author: "AI", message: "Wall-E ?" },
     { author: "Human", message: "In Star Wars" },
     { author: "AI", message: "R2D2 ?" },
     { author: "Human", message: "The one who talks a lot" },
     { author: "AI", message: "C3PO !" },
-  ]
+]
+
+const useStyles = makeStyles({
+  root: {
+    padding: '24px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    // TODO should not do that on desktop, might by useless with design complete (with bubbles...)
+    // to be able to place play button at the end of the screen size
+    // // so root div take at least full screen size (https://stackoverflow.com/questions/2363455/making-div-height-at-least-as-tall-as-the-page-in-css)
+    minHeight: '100vh',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #fffb1e 90%)'
+  },
+  // linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+  // background: '#fffb1e'
+  title: {
+    marginBottom: '24px',
+    fontFamily: '\'Permanent Marker\', cursive'
+  },
+  subtitle: {
+    marginBottom: '24px'
+  },
+  discussionContainer: {
+
+  },
+  playButton: {
+    width: '100%', // TODO make it full size only on mobile
+    fontFamily: '\'Permanent Marker\', cursive'
+  },
+  playButtonContainer: {
+    marginTop: 'auto', // to place button at the bottom of the screen
+  }
+
+});
+
+function Intro(): JSX.Element {
+
+  const classes = useStyles()
+
   return (
-    <div>
-        <Typography variant="h2">
-            Heading H2
+    <div className={classes.root}>
+        <Typography variant="h1" align="center" className={classes.title}>
+            Guess It AI!
         </Typography>
-      <div>Can an AI guess what you are talking about ?</div>
-      <br/>
-      <Discussion messages={messages}/>
-      <br/>
-      <Link to={boardRoute}><button>Let's play !</button></Link>
+        <Typography variant="h3" align="center" className={classes.subtitle} component="h2">
+            Can an AI guess what you are talking about ?
+        </Typography>
+      <div className={classes.discussionContainer}>
+        <Discussion messages={demoMessages}/>
+      </div>
+      <div className={classes.playButtonContainer}>
+        <Link to={boardRoute}><Button variant="contained" color="primary" className={classes.playButton}>Let's play !</Button></Link>
+      </div>
     </div>
   )
 }
