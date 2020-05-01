020065a7f294e8d8c52fd03a28f146bc86278823
Nicolas <nicolas.lecrique+git@globers.co>
2020-05-01 08:21:52 +0200

    add scrolling behavior on conversation (auto scroll to botom)

--- front/src/component/Discussion.tsx
+++ front/src/component/Discussion.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import {Message, MessageProps} from './Message'
 import {makeStyles} from '@material-ui/core/styles'
 
@@ -15,16 +15,32 @@ const useStyles = makeStyles({
     backgroundColor: 'rgba(255,255,255,0.3)',
     borderRadius: '24px',
     padding: '24px 4px 24px 4px',
+    overflow: 'hidden',
+    display: 'flex',
+    flexDirection: 'column',
   }
 })
 
 export function Discussion({ messages }: DiscussionProps): JSX.Element {
   const classes = useStyles()
+
+  // cf. https://stackoverflow.com/questions/37620694/how-to-scroll-to-bottom-in-react
+  // we need a "useRef" because scrollIntoView applies on dom element which doesn't exists yet when "render" is called (still in virtual dom)
+  const messagesEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+    // @ts-ignore
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(scrollToBottom, [messages]);
+
   return (
     <div className={classes.root}>
       {messages.map((message, index) => {
         return (<Message key={index} author={message.author} message={message.message} aiConfidence={message.aiConfidence}/>)
       })}
+      <div ref={messagesEndRef} />
     </div>
   )
 }
