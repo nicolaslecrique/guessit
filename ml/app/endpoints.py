e5575c95af872823b5a2b3c455846fbeb7e6f27e
Mokhtari Mohamed <mohamed.mokhtari2@gmail.com>
2020-05-15 17:31:58 +0200

    - Add a new sentences POST endpoint - Entity uri is no more necessary while guessing - Factorize code on the guesser

--- ml/app/endpoints.py
+++ ml/app/endpoints.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
 from starlette.requests import Request
 
-from app.schemas import GuessEntityQuery, GuessEntityReply
+from app.schemas import GuessEntityQuery, GuessEntityReply, SentencesQuery
 
 
 router = APIRouter()
 
 @router.post("/guess_entity", response_model=GuessEntityReply)
 def guess_entity(request: Request, query: GuessEntityQuery) -> GuessEntityReply:
-    guesses = request.app.state.guesser.guess(query.entityToGuessUri, query.descriptionSentences)
+    guesses = request.app.state.guesser.guess(query.descriptionSentences)
 
     return GuessEntityReply(guesses=guesses)
+
+@router.post("/sentences", response_model=None)
+def sentences(request: Request, query: SentencesQuery) -> None:
+    request.app.state.guesser.add_sentences(query.entityToGuessUri, query.descriptionSentences)
--- ml/app/schemas.py
+++ ml/app/schemas.py
@@ -4,8 +4,11 @@ from pydantic import BaseModel # pylint: disable=no-name-in-module
 
 
 class GuessEntityQuery(BaseModel):
-    entityToGuessUri: str
     descriptionSentences: List[str]
 
 class GuessEntityReply(BaseModel):
     guesses: Dict[str, float]
+
+class SentencesQuery(BaseModel):
+    entityToGuessUri: str
+    descriptionSentences: List[str]
