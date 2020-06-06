05edbf3804f06321cbd8bd51feb95080ec227b01
Nicolas <nicolas.lecrique+git@globers.co>
2020-06-06 16:37:37 +0200

    remove unused script

--- ml/deploy_to_cloud_run.sh
+++
@@ -1,4 +0,0 @@
-# build docker image then upload it to google container registry
-gcloud builds submit --tag gcr.io/ibo-speak/ml
-# deploy image from container registry to google cloud run
-gcloud run deploy --image gcr.io/ibo-speak/ml --platform managed
